9f504387879a5afddf8f99c4ce70becf52741d0b
SebasOrtiz <sebastian.santacoloma@agriculturaverion.ind.br>
2024-03-02 19:06:25 -0300

    Add Dialog Content

---
+++ MusicStoreApp/Views/MusicStoreView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MusicStoreApp.Views;
+
+public partial class MusicStoreView : UserControl
+{
+    public MusicStoreView()
+    {
+        InitializeComponent();
+    }
+}
--- MusicStoreApp/obj/Debug/net6.0/MusicStoreApp.AssemblyInfo.cs
+++ MusicStoreApp/obj/Debug/net6.0/MusicStoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicStoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e632ea2d733edae548bac23783c9ded5d80b5a11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e726a6b9eba059257f566ce44d2d3086c3937013")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicStoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicStoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

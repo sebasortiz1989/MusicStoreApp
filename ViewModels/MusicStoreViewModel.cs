54d94b3c74cd5c4d5ddb2dbdbb1c9e43e414a1dc
SebasOrtiz <sebastian.santacoloma@agriculturaverion.ind.br>
2024-03-02 19:11:55 -0300

    Mock Search

--- ViewModels/MusicStoreViewModel.cs
+++ ViewModels/MusicStoreViewModel.cs
@@ -1,6 +1,38 @@
-﻿namespace MusicStoreApp.ViewModels;
+﻿using System.Collections.ObjectModel;
+using ReactiveUI;
 
-public class MusicStoreViewModel
+namespace MusicStoreApp.ViewModels;
+
+public class MusicStoreViewModel : ViewModelBase
 {
-    
+    private string? _searchText;
+    private bool _isBusy;
+    private AlbumViewModel? _selectedAlbum;
+
+    public MusicStoreViewModel()
+    {
+        SearchResults.Add(new AlbumViewModel());
+        SearchResults.Add(new AlbumViewModel());
+        SearchResults.Add(new AlbumViewModel());
+    }
+
+    public ObservableCollection<AlbumViewModel> SearchResults { get; } = new();
+
+    public AlbumViewModel? SelectedAlbum
+    {
+        get => _selectedAlbum;
+        set => this.RaiseAndSetIfChanged(ref _selectedAlbum, value);
+    }
+
+    public string? SearchText
+    {
+        get => _searchText;
+        set => this.RaiseAndSetIfChanged(ref _searchText, value);
+    }
+
+    public bool IsBusy
+    {
+        get => _isBusy;
+        set => this.RaiseAndSetIfChanged(ref _isBusy, value);
+    }
 }

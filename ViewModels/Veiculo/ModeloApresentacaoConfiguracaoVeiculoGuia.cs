7a5326a83be0aa2ab4ead320370c287c979b333e
SebasOrtiz <sebasortizsanta@gmail.com>
2024-04-12 11:55:22 -0300

    Sebas: [add] Done tela configuracao principal

--- ViewModels/Veiculo/ModeloApresentacaoConfiguracaoVeiculoGuia.cs
+++ ViewModels/Veiculo/ModeloApresentacaoConfiguracaoVeiculoGuia.cs
@@ -1,24 +1,8 @@
-﻿using System.Collections.ObjectModel;
-using Verion.Presentation.View;
-
-namespace MusicStoreApp.ViewModels.Veiculo;
+﻿namespace MusicStoreApp.ViewModels.Veiculo;
 
 public class ModeloApresentacaoConfiguracaoVeiculoGuia : ViewModelBase
 {
     public ModeloApresentacaoConfiguracaoVeiculoGuia()
     {
-
-        NomeBotoesSeletor = new ObservableCollection<InfoBotoes>
-        {
-            new("Configurações", SvgIcons.ImageSvg, new SynchronizedCommand(() => { }, SynchronizationBehavior.Discard, true)),
-            new("Veículo", SvgIcons.ImageSvg, new SynchronizedCommand(() => { }, SynchronizationBehavior.Discard, true)),
-            new("GPS", SvgIcons.ImageSvg, new SynchronizedCommand(() => { }, SynchronizationBehavior.Discard, true)),
-            new("Trabalho", SvgIcons.ImageSvg, new SynchronizedCommand(() => { }, SynchronizationBehavior.Discard, true)),
-            new("Dados", SvgIcons.ImageSvg, new SynchronizedCommand(() => { }, SynchronizationBehavior.Discard, true)),
-            new("Meu Aplicativo", SvgIcons.ImageSvg, new SynchronizedCommand(() => { }, SynchronizationBehavior.Discard, true)),
-            new("Voltar", SvgIcons.ImageSvg, new SynchronizedCommand(() => { }, SynchronizationBehavior.Discard, true)),
-        };
     }
-    
-    public ObservableCollection<InfoBotoes> NomeBotoesSeletor { get; }
 }
--- Views/Veiculo/TelaConfiguracaoPrincipal.axaml.cs
+++ Views/Veiculo/TelaConfiguracaoPrincipal.axaml.cs
@@ -1,12 +1,87 @@
-﻿using Avalonia.ReactiveUI;
+﻿using System.Collections.ObjectModel;
+using Avalonia.ReactiveUI;
 using MusicStoreApp.ViewModels.Veiculo;
+using Verion.Apresentacao.Avalonia.Buttons;
 
 namespace MusicStoreApp.Views.Veiculo;
 
 public partial class TelaConfiguracaoPrincipal : ReactiveWindow<ModeloApresentacaoConfiguracaoVeiculoGuia>
 {
+    private Collection<CustomButton> buttonList;
+
     public TelaConfiguracaoPrincipal()
     {
         InitializeComponent();
+        buttonList = new Collection<CustomButton> { BottaoVeiculo, BotaoGps, BotaoTrabalho, BotaoDados, BotaoMeuAplicativo };
+        BottaoVeiculo.IsCheckedChanged += OnIsCheckedChangedVeiculoEvent;
+        BotaoGps.IsCheckedChanged += OnIsCheckedChangedGpsEvent;
+        BotaoTrabalho.IsCheckedChanged += OnIsCheckedChangedTrabalhoEvent;
+        BotaoDados.IsCheckedChanged += OnIsCheckedChangedDadosEvent;
+        BotaoMeuAplicativo.IsCheckedChanged += OnIsCheckedChangedMeuAplicativoEvent;
+    }
+
+    private void OnIsCheckedChangedVeiculoEvent(CustomButton customButton, bool arg2)
+    {
+        if (customButton is not { IsChecked: true }) return;
+
+        foreach (var button in buttonList)
+        {
+            if (button.ButtonText != customButton.ButtonText)
+                button.IsChecked = false;
+        }
+        
+        // ContentControlConfiguracao.Content =
+    }
+
+    private void OnIsCheckedChangedGpsEvent(CustomButton customButton, bool arg2)
+    {
+        if (customButton is not { IsChecked: true }) return;
+
+        foreach (var button in buttonList)
+        {
+            if (button.ButtonText != customButton.ButtonText)
+                button.IsChecked = false;
+        }
+
+        // ContentControlConfiguracao.Content =
+    }
+
+    private void OnIsCheckedChangedTrabalhoEvent(CustomButton customButton, bool arg2)
+    {
+        if (customButton is not { IsChecked: true }) return;
+
+        foreach (var button in buttonList)
+        {
+            if (button.ButtonText != customButton.ButtonText)
+                button.IsChecked = false;
+        }
+
+        // ContentControlConfiguracao.Content =
+    }
+
+    private void OnIsCheckedChangedDadosEvent(CustomButton customButton, bool arg2)
+    {
+        if (customButton is not { IsChecked: true }) return;
+
+        foreach (var button in buttonList)
+        {
+            if (button.ButtonText != customButton.ButtonText)
+                button.IsChecked = false;
+        }
+
+        // ContentControlConfiguracao.Content =
+    }
+
+    private void OnIsCheckedChangedMeuAplicativoEvent(CustomButton customButton, bool arg2)
+    {
+        if (customButton is not { IsChecked: true }) return;
+
+        foreach (var button in buttonList)
+        {
+            if (button.ButtonText != customButton.ButtonText)
+                button.IsChecked = false;
+        }
+
+        // ContentControlConfiguracao.Content =
     }
 }
